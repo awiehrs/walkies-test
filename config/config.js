4ee8e8f84c563e350e62d7ecca032e31bae2fba3
Amanda <awiehrs.photo@gmail.com>
2021-03-16 16:49:07 -0400

    env files now match config, localhost is connecting

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 /* eslint-disable prettier/prettier */
-console.log("hit config.js");
+
 module.exports = {
   "development": {
-    "username": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_KEY,
-    "database": process.env.MYSQL_DBNAME,
-    "host": process.env.MYSQL_HOST,
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
--- server.js
+++ server.js
@@ -2,14 +2,11 @@ require("dotenv").config();
 
 const express = require("express");
 
-
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 const db = require("./models");
-console.log("got past require/models line 9")
 app.use(express.static("public"));
-
 
 //ENABLE if you plan to use handlebars.
 const exphbs = require("express-handlebars");
@@ -18,9 +15,16 @@ app.set("view engine", "handlebars");
 
 //ENABLE if you plan to use controllers as routes/orm.
 require("./controllers/html-routes.js")(app);
-console.log("got past html-routes reference")
-db.sequelize.sync({}).then(() => {
+console.log("got past html-routes reference");
+console.log("this is the line right after");
+
+db.sequelize.sync().then(() => {
+  console.log("inside the sync function");
   app.listen(PORT, () => {
-    console.log("Listening on localhost:" + PORT);
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
   });
 });
